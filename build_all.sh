ce911e4cb7ac0c3c9147b8a1ae25762a56381297
celestria27 <adrianjohn.aspiras@gmail.com>
2026-06-19 14:54:44 +0800

    updated to latest zmk

--- build_all.sh
+++ build_all.sh
@@ -25,13 +25,13 @@ fi
 cd app
 
 # Build Dongle
-west build -p -d build/dongle -b nice_nano_v2 -S studio-rpc-usb-uart -- -DSHIELD="fifi_dongle_pro_micro dongle_display" -DZMK_EXTRA_MODULES="/home/celestria/repositories/keyboard/zmk-config-kurofi;/home/celestria/repositories/keyboard/zmk-dongle-display" -DZMK_CONFIG="/home/celestria/repositories/keyboard/zmk-config-kurofi/config"
+west build -p -d build/dongle -b nice_nano -S studio-rpc-usb-uart -- -DSHIELD="fifi_dongle_pro_micro dongle_display" -DZMK_EXTRA_MODULES="/home/celestria/repositories/keyboard/zmk-config-kurofi;/home/celestria/repositories/keyboard/zmk-dongle-display" -DZMK_CONFIG="/home/celestria/repositories/keyboard/zmk-config-kurofi/config"
 
 # Build Right
-west build -p -d build/right -b nice_nano_v2 -- -DSHIELD="fifi_right" -DZMK_EXTRA_MODULES="/home/celestria/repositories/keyboard/zmk-config-kurofi" -DZMK_CONFIG="/home/celestria/repositories/keyboard/zmk-config-kurofi/config"
+west build -p -d build/right -b nice_nano -- -DSHIELD="fifi_right" -DZMK_EXTRA_MODULES="/home/celestria/repositories/keyboard/zmk-config-kurofi" -DZMK_CONFIG="/home/celestria/repositories/keyboard/zmk-config-kurofi/config"
 
 # Build Left 
-west build -p -d build/left -b nice_nano_v2 -- -DSHIELD="fifi_left_peripheral" -DZMK_EXTRA_MODULES="/home/celestria/repositories/keyboard/zmk-config-kurofi" -DZMK_CONFIG="/home/celestria/repositories/keyboard/zmk-config-kurofi/config"
+west build -p -d build/left -b nice_nano -- -DSHIELD="fifi_left_peripheral" -DZMK_EXTRA_MODULES="/home/celestria/repositories/keyboard/zmk-config-kurofi" -DZMK_CONFIG="/home/celestria/repositories/keyboard/zmk-config-kurofi/config"
 
 if [ ${VIRTUAL_ENV} ] ; then
     deactivate
